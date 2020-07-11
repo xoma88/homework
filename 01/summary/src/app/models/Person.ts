572283b0f7134b47df6d63cc9c1d0eb720e52f20
xoma88 <homich8897@gmail.com>
2020-07-12 02:14:45 +0300

    summary finish

---
+++ 01/summary/src/app/models/Person.ts
@@ -0,0 +1,22 @@
+export class Person {
+    name = 'Имя';
+    surname = 'Фамилия';
+    patronymic = 'Отчество';
+    age = 32;
+    dateOfBirth = '01.01.1988';
+
+    city = 'Город';
+    country = 'Страна';
+    strit = 'Улица';
+    houseNumber = 12;
+    apartmentNumber = 6;
+
+    education = 'Высшее';
+    specialty = 'Специальность';
+
+    workCompany = [['место работы1'], ['место работы2'], ['место работы3']];
+    workPosition = [' должность1', ' должность2', ' должность3'];
+    workYearsOfWork =  [' 2008-2010', ' 2010-2015', ' 2015-2019'];
+
+    aboutMe = 'О себе, О себе, О себе, О себе, О себе, О себе, О себе, О себе, О себе';
+}
